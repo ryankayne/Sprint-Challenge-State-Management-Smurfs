f69e79f021af54dd2a4ed2ee532bc9f5cb69b304
ryankayne <coope1rk@gmail.com>
2019-12-13 11:42:07 -0500

    Initial Commit, Redux setup actually

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './reducer/reducer';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById("root"));
